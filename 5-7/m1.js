32de9ab34b020d5bdc4cfc853ff9a8ca5bc3e76e
2009month7 <1824618385@qq.com>
2021-05-08 11:04:40 +0800

    class

---
+++ 5-7/m1.js
@@ -0,0 +1,7 @@
+let name = "zhangsan";
+let obj = {
+    name : "常洪阳",
+    age : 18,
+    sex : "W"
+}
+export {name,obj}
---
+++ 5-7/m2.js
@@ -0,0 +1,3 @@
+import {name,obj} from "./m1.js"
+// console.log(name);
+// console.log(obj);
